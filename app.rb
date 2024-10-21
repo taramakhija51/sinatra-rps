202c7438be7293445ec81dd8eefca179e166504f
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-10-21 13:56:40 +0000

    index page and routes created

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
+  erb(:index)
+end
+=begin
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/scissors") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/paper") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+=end
